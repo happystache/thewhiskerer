116d499bfcd2cb0370723aec95d2c7cd7087abd2
Dennis Thompson <jib1dx6u@students.codeinstitute.net>
2024-11-14 03:27:14 +0000

    adding main

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,3 @@
 // JavaScript to toggle the navigation menu on mobile
 document.getElementById('hamburger-icon').addEventListener('click', function() {
-    document.querySelector('.nav-menu').classList.toggle('open');
+    document.querySelector('.nav-menu').classList.toggle('open');}
